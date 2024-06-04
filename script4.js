46af89b9fe5a8dc0c2fed39886fe7e3d6e49a4d8
palaksingh013 <152655795+palaksingh013@users.noreply.github.com>
2024-06-04 23:05:10 +0530

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Sticky navigation
+window.onscroll = function () {
+  stickyNav();
+};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
